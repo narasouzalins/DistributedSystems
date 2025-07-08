77a2b8e8160b21944eee4c92281f15521e37504b
Nara Lins <naralinsie@gmail.com>
2025-07-08 17:24:00 +0100

    finish creating servers and fixing some errors

---
+++ src/main/java/CourseServiceServer.java
@@ -0,0 +1,93 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import org.womenpower.courses.*;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class CourseServiceServer { //method to initiate the server
+    public static void main(String[] args) throws Exception {
+        Server sv = ServerBuilder.forPort(8091)
+                .addService(new CourseEnrollmentImpl()) //to register implementation
+                .build();
+        System.out.println( "Starting Course Service Server");
+
+        sv.start(); //to initiate server
+        sv.awaitTermination();  //keep the server running
+    }
+
+            //CourseService implementation
+    static class CourseEnrollmentImpl extends CourseServiceGrpc.CourseServiceImplBase {
+
+                /*
+                 * enrollment implementation
+                 * @param request userId and courseId
+                 */
+        @Override
+        public void enrollCourse(CourseEnrollment request, StreamObserver<EnrollmentStatus> responseObserver) {
+
+            String userId = request.getUserId();
+            String courseId = request.getCourseId();
+            // to validate user
+            if(userId.isEmpty() || courseId.isEmpty()) {
+                responseObserver.onNext(EnrollmentStatus.newBuilder()
+                        .setEnrollmentId("Error: UserId or CourseId are required")
+                        .build());
+                responseObserver.onCompleted();
+                return;
+            }
+                    // to generate an unique ID
+            String enrollmentId = "ID" + UUID.randomUUID().toString();
+            String courseLink = "CourseLink" + courseId;
+
+            responseObserver.onNext(EnrollmentStatus.newBuilder()
+                    .setEnrollmentId(userId)
+                    .setCourseLink(courseId)
+                    .build());
+            responseObserver.onCompleted();
+        }
+                    /* generateCert implementation
+                    @param request enrollmentID and AssessmentScore
+                     */
+        @Override
+        public void generateCertificate(CertificateRequest request, StreamObserver<Certificate> responseObserver) {
+            String enrollmentId = request.getEnrollmentId();
+            String assessmentScore = request.getAssessmentScore();
+
+            //validation
+            if(enrollmentId.isEmpty() || assessmentScore.isEmpty()) {
+                responseObserver.onNext(Certificate.newBuilder()
+                        .setPdfUrl("Error: Missing enrollment ID or assessment score")
+                        .build());
+                responseObserver.onCompleted();
+                return;
+            }
+            //verify if the score is only numbers
+            if (!assessmentScore.matches("\\d+")){
+                responseObserver.onNext(Certificate.newBuilder()
+                .setPdfUrl("Error: The score must be a number")
+                        .build());
+                responseObserver.onCompleted();
+                return;
+            }
+            // Convert to number
+            int score = Integer.parseInt(assessmentScore);
+
+            //verify min score
+            if(score < 70) {
+                responseObserver.onNext(Certificate.newBuilder()
+                        .setPdfUrl("Error: The score must be at least 70")
+                        .build());
+            }else{
+                //to generate certificate for approved people
+                String pdfUrl = "http://localhost:8090/pdfs/" + enrollmentId + ".pdf";
+                responseObserver.onNext(Certificate.newBuilder()
+                        .setPdfUrl(pdfUrl)
+                        .build());
+            }
+            responseObserver.onCompleted();
+        }
+
+    }
+}
---
+++ src/main/java/JobHubServer.java
@@ -0,0 +1,72 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import org.womenpower.jobhub.*;
+
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JobHubServer {   //this method is to initiate the server
+    public static void main(String[] args) throws Exception {
+        Server server = ServerBuilder.forPort(8092)
+                .addService( new JobHubService())
+                .build()
+                .start();
+        System.out.println("JobHubService started on port 8092");
+        server.awaitTermination();
+    }
+                   //service implementation
+    static class JobHubService extends JobHubServiceGrpc.JobHubServiceImplBase {
+        List<Mentorship> mentours = Arrays.asList(
+                Mentorship.newBuilder()
+                        .setMentorName("Ann Fanegan")
+                        .setExpertise("Software Engineer")
+                        .setContactInfo("ann@gmail.com")
+                        .build()
+
+        );    //database
+        List<JobListing> jobs = Arrays.asList(
+                JobListing.newBuilder()
+                        .setJobTitle("Java Developer")
+                        .setCompany("Google")
+                        .setSalaryRange(5000)
+                        .build()
+        );
+
+        @Override
+        public void findMentorship(MentorshipQuery request, StreamObserver<Mentorship> responseObserver) {
+            for (Mentorship m : mentours) {
+                if (request.getInterestsList().isEmpty() || m.getExpertise().toLowerCase().contains(request.getInterestsList().get(0).toLowerCase())) {
+                    responseObserver.onNext(m);
+                }
+            }
+            responseObserver.onCompleted();
+            System.out.println("Mentorship found");
+        }
+                //MatchJOb service implementation (unary)
+        @Override
+        public void matchJob(JobMatchRequest request, StreamObserver<JobListing> responseObserver) {
+            System.out.println("Searching for job for: " + request.getUserId());
+         //Search for the first vacancy that matches:
+        // Salary >= minimum requested
+        // Location contains the filter
+            for (JobListing j : jobs) {
+                if (j.getSalaryRange() >= request.getMinSalary() &&
+                        (request.getLocation().isEmpty() ||
+                        j.getCompany().toLowerCase()
+                        .contains(request.getLocation().toLowerCase()))) {
+
+                    //Found a matched job and send to user
+                    responseObserver.onNext(j);
+                    responseObserver.onCompleted();
+                    System.out.println("Job found" + j.getJobTitle());
+                    return;
+                }
+            }//if not matches found
+            responseObserver.onCompleted();
+            System.out.println("No jobs found");
+        }
+    }
+}
